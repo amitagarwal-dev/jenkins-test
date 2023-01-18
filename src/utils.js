c349ebfbbe955c4e3684fc78041f1f13f0f8dafa
Amit Agarwal <amitagarwal@integramicro.com>
2023-01-18 17:08:07 +0530

    jenkins file updated

--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,18 @@ function sumAsync(a,b){
         },1500)
     })
 }
+// function sumAsync(a,b){
+//     return new Promise((resolved,rejected)=>{
+//         setTimeout(()=>{
+//             return resolved(a+b);
+//         },1500)
+//     })
+// }
 
+let a = true;
+if(a==true){
+    console.log(a);
+}
 module.exports = {
     sumSync,
     sumAsync
