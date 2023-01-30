74f0a5dd86b161101b643e66732fc42df119d07e
Amit Agarwal <amitagarwal@integramicro.com>
2023-01-30 12:12:44 +0530

    code updated

--- src/utils.js
+++ src/utils.js
@@ -16,6 +16,20 @@ function sumAsync(a,b){
 //     })
 // }
 
+// function sumAsync(a,b){
+//     return new Promise((resolved,rejected)=>{
+//         setTimeout(()=>{
+//             return resolved(a+b);
+//         },1500)
+//     })
+// }
+// function sumAsync(a,b){
+//     return new Promise((resolved,rejected)=>{
+//         setTimeout(()=>{
+//             return resolved(a+b);
+//         },1500)
+//     })
+// }
 let a = true;
 if(a==true){
     console.log(a);
